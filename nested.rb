e883b7c972cfe245e2fc4a12c89be584d980137e
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-17 23:03:57 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].join
+     
 end
 
 def adding_matz
@@ -79,7 +84,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  
+  programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
+  
+  p programmer_hash
     
 end
 
@@ -102,6 +110,10 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
+     p programmer_hash
+     
      
 end
 
@@ -121,5 +133,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     
+     p programmer_hash
 
 end
